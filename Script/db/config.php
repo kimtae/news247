7b9f0be6b177e2fa160b1a0e4a199a5a504e930d
Bwire Mashauri <35838987+255programmer@users.noreply.github.com>
2021-09-18 13:27:13 +0300

    Add files via upload

---
+++ Script/db/config.php
@@ -0,0 +1,22 @@
+<?php
+//Database Server Name:
+DEFINE('DBHost','YOUR SEVER NAME');
+
+//Database Username:
+DEFINE('DBUser', 'YOUR USERNAME');
+
+//Database Password:
+DEFINE('DBPass','YOUR PASSWORD');
+
+//Database Name:
+DEFINE('DBName','YOUR DB NAME');
+
+//Character set:
+DEFINE('DBCharset','utf8mb4');
+
+//Database Collation:
+DEFINE('DBCollation', 'utf8_general_ci');
+
+//Database Prefix:
+DEFINE('DBPrefix', '');
+?>
